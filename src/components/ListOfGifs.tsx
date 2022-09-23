f5ff0384745167e7e318522ec5e3cf2c04eba8bb
Miguel Angel Abad <miguel.abad86@gmail.com>
2022-09-23 22:37:01 +0200

    Create loading effect via useState

--- src/components/ListOfGifs.tsx
+++ src/components/ListOfGifs.tsx
@@ -2,19 +2,22 @@ import { useEffect, useState } from 'react';
 import getGifs from '../services/getGifs';
 import { Gif } from './Gif';
 
-interface GifParams {
-  params: {
-    keyword: string
-  }
-}
+interface GifParams { params: { keyword: string } }
 
 export const ListOfGifs = ({ params }: GifParams) => {
+  const [loading, setLoading] = useState(false)
   const [gifs, setGifs] = useState<any>([]);
   const { keyword } = params;
 
   useEffect(() => {
-    getGifs({ keyword }).then((gifs) => setGifs(gifs));
+    setLoading(true)
+    getGifs({ keyword }).then((gifs) => {
+      setGifs(gifs)
+      setLoading(false)
+    });
   }, [keyword]);
+
+  if (loading) return <span>Loading...🌀</span>
 
   return gifs.map(({ title, url, id }: any) => (
     <Gif key={id} title={title} url={url} id={id} />
